bb459109996d14c5553541b2823c816203a17735
Gabriel Beltran <gabrielaudbeltran23@gmail.com>
2022-09-02 22:01:07 -0600

    More commits regarding rendering through hbs

--- pracserver/routes/bullshittery.js
+++ pracserver/routes/bullshittery.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 //THIS IS THE NON-TEMPLATE ISH
 router.get('/', function (req,res, next)
@@ -13,10 +17,26 @@ router.get('/', function (req,res, next)
     <body>
         <div class="container">
             <img src="https://thumbs.gfycat.com/AgilePoliticalAfricanparadiseflycatcher-size_restricted.gif" alt=""/>
+            <a class="btn btn-primary" href="/bullshittery/second-level">Second-level bullshittery</a>
         </div>
     </body>
     </html>
     `)
+})
+
+router.get('/second-level', function (req, res, next)
+{
+    let names = ["Tass", "Levi", "Matt", "Taylor", "Krishiv", "Kale", "Cody", "Ethan"];
+    let user = "Gabe";
+
+    res.render('simple-hell', {
+
+        name: user,
+        userslog: names,
+        isgabe: user === "Gabe",
+        securitylevel: "epic"
+
+    })
 })
 
 //Let's make it available!
--- pracserver/routes/index.js
+++ pracserver/routes/index.js
@@ -9,4 +9,5 @@ router.get('/', function(req, res, next) {
   });
 });
 
+//The middleman
 module.exports = router;
